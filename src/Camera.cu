40ddcd3dcb14f035fe4d47777afacc94b808db8f
Henry LE BERRE <24208000+henryleberre@users.noreply.github.com>
2020-10-20 22:42:26 +0200

    Added RenderParams Class

--- src/Camera.cu
+++ src/Camera.cu
@@ -2,6 +2,9 @@
 #define __PRTX__CAMERA_CU
 
 #include "Math.cu"
+#include "Misc.cu"
+#include "Memory.cu"
+#include "Random.cu"
 
 namespace PRTX {
 
@@ -9,6 +12,27 @@ namespace PRTX {
         float   fov;
         ::PRTX::Vec4f32 position;
     }; // Camera
+
+    struct RenderParams {
+        size_t width  = 0;
+        size_t height = 0;
+
+        ::PRTX::Camera camera;
+    };
+
+    __device__ inline Ray GenerateCameraRay(const size_t& pixelX, const size_t& pixelY, const ::PRTX::GPU_ptr<::PRTX::RenderParams>& pRanderParams) noexcept {
+        const ::PRTX::RenderParams& renderParams = *pRanderParams;
+
+        Ray ray;
+        ray.origin    = Vec4f32(0.f, 0.f, 0.f, 0.f);
+        ray.direction = Vec4f32(
+            2.0f  * ((pixelX + RandomFloat()) / float(renderParams.width)  - 1.0f) * tan(renderParams.camera.fov) * renderParams.width / renderParams.height,
+            -2.0f * ((pixelY + RandomFloat()) / float(renderParams.height) + 1.0f) * tan(renderParams.camera.fov),
+            1.0f,
+            0.f);
+      
+        return ray;
+    }
 
 }; // namespace PRTX
 
--- src/PolarTracer.cu
+++ src/PolarTracer.cu
@@ -1,6 +1,7 @@
 #ifndef __PRTX__POLAR_TRACER_CU
 #define __PRTX__POLAR_TRACER_CU
 
+#include "Misc.cu"
 #include "Math.cu"
 #include "Image.cu"
 #include "Memory.cu"
@@ -13,20 +14,6 @@ namespace PRTX {
 
     namespace details {
 
-        struct PolarTracerNonMeshData {
-            size_t width  = 0;
-            size_t height = 0;
-
-            Camera camera{};
-            
-            // constants computed with the "camera" object
-            float cameraProjectW = 0;
-            float cameraProjectH = 0;
-        };
-
-        template <typename _T>
-        __device__ inline _T Clamp(const _T& x, const _T& min, const _T& max) noexcept { return min(max(x, min), max); }
-
         template <size_t _N>
         __device__ Colorf32 RayTrace(const Ray& ray) {
             const size_t pixelX = threadIdx.x + blockIdx.x * blockDim.x;
@@ -35,35 +22,19 @@ namespace PRTX {
             return Colorf32(pixelX / (float)1920, pixelY / (float)1080, 0.0f, 1.0f);
         }
 
-        __device__ __host__ inline float RandomFloat() noexcept {
-            return 0.5f;
-        }
-
-        __device__ inline Ray GenerateCameraRay(const size_t& pixelX, const size_t& pixelY, const ::PRTX::GPU_ptr<PolarTracerNonMeshData>& pDeviceNMD) noexcept {
-            Ray ray;
-            ray.origin    = Vec4f32(0.f, 0.f, 0.f, 0.f);
-            ray.direction = Vec4f32(
-                (2.0f  * ((pixelX + RandomFloat()) / float(pDeviceNMD->width)  - 1.0f) * pDeviceNMD->cameraProjectW),
-                (-2.0f * ((pixelY + RandomFloat()) / float(pDeviceNMD->height) + 1.0f) * pDeviceNMD->cameraProjectH),
-                1.0f,
-                0.f);
-          
-            return ray;
-        }
-
         // Can't pass arguments via const& because these variables exist on the host and not on the device
-        __global__ void RayTracingDispatcher(const ::PRTX::Pointer<Coloru8, ::PRTX::Device::GPU> pSurface, const PRTX::GPU_ptr<PolarTracerNonMeshData> pDeviceNMD) {
+        __global__ void RayTracingDispatcher(const ::PRTX::Pointer<Coloru8, ::PRTX::Device::GPU> pSurface, const PRTX::GPU_ptr<::PRTX::RenderParams> pParams) {
             // Calculate the thread's (X, Y) location
             const size_t pixelX = threadIdx.x + blockIdx.x * blockDim.x;
             const size_t pixelY = threadIdx.y + blockIdx.y * blockDim.y;
 
             // Bounds check
-            if (pixelX >= pDeviceNMD->width || pixelY >= pDeviceNMD->height) return;
+            if (pixelX >= pParams->width || pixelY >= pParams->height) return;
 
             // Determine the pixel's index into the image buffer
-            const size_t index = pixelX + pixelY * pDeviceNMD->width;
+            const size_t index = pixelX + pixelY * pParams->width;
 
-            const Ray cameraRay = GenerateCameraRay(pixelX, pixelY, pDeviceNMD);
+            const Ray cameraRay = GenerateCameraRay(pixelX, pixelY, pParams);
 
             // the current pixel's color (represented with floating point components)
             Colorf32 pixelColor = RayTrace<0>(cameraRay) * 255.f;
@@ -77,42 +48,38 @@ namespace PRTX {
     class PolarTracer {
     private:
         struct {
-            ::PRTX::details::PolarTracerNonMeshData m_nonMeshData;
+            ::PRTX::RenderParams m_renderParams;
         } host;
 
         struct {
             ::PRTX::Image<Coloru8, ::PRTX::Device::GPU> m_frameBuffer;
-            ::PRTX::GPU_ptr<::PRTX::details::PolarTracerNonMeshData> m_pNonMeshData;
+            ::PRTX::GPU_ptr<::PRTX::RenderParams> m_pRenderParams;
         } device;
     
     public:
-        PolarTracer(const size_t width, const size_t height, const Camera& camera, const ::PRTX::CPU_Array<Sphere>& spheres) {
-            this->host.m_nonMeshData.width  = width;
-            this->host.m_nonMeshData.height = height;
-            this->host.m_nonMeshData.camera = camera;
-            this->host.m_nonMeshData.cameraProjectH = std::tan(camera.fov);
-            this->host.m_nonMeshData.cameraProjectW = this->host.m_nonMeshData.cameraProjectH * width / height;
+        PolarTracer(const ::PRTX::RenderParams& renderParams, const ::PRTX::CPU_Array<Sphere>& spheres)
+            : host{renderParams}
+        {
+            this->device.m_frameBuffer   = ::PRTX::Image<Coloru8, ::PRTX::Device::GPU>(renderParams.width, renderParams.height);
+            this->device.m_pRenderParams = ::PRTX::AllocateCount<::PRTX::RenderParams, PRTX::Device::GPU>(1);
 
-            this->device.m_frameBuffer  = ::PRTX::Image<Coloru8, ::PRTX::Device::GPU>(width, height);
-            this->device.m_pNonMeshData = ::PRTX::AllocateCount<::PRTX::details::PolarTracerNonMeshData, PRTX::Device::GPU>(1);
-
-            const auto src = ::PRTX::CPU_ptr<::PRTX::details::PolarTracerNonMeshData>(&this->host.m_nonMeshData);
-            ::PRTX::CopySize(this->device.m_pNonMeshData, src, sizeof(::PRTX::details::PolarTracerNonMeshData));
+            const auto src = ::PRTX::CPU_ptr<::PRTX::RenderParams>(&this->host.m_renderParams);
+            ::PRTX::CopySize(this->device.m_pRenderParams, src, sizeof(::PRTX::RenderParams)); // TODO:: use CopyCount
         }
 
         inline void RayTraceScene(const ::PRTX::Image<::PRTX::Coloru8, ::PRTX::Device::CPU>& outSurface) {
-            assert(outSurface.GetWidth() == this->host.m_nonMeshData.width && outSurface.GetHeight() == this->host.m_nonMeshData.height);
+            assert(outSurface.GetWidth() == this->host.m_renderParams.width && outSurface.GetHeight() == this->host.m_renderParams.height);
 
             const size_t bufferSize = outSurface.GetPixelCount() * sizeof(Coloru8);
     
             // Allocate 1 thread per pixel of coordinates (X,Y). Use as many blocks in the grid as needed
             // The RayTrace function will use the thread's index (both in the grid and in a block) to determine the pixel it will trace rays through
             const dim3 dimBlock = dim3(32, 32); // 32 warps of 32 threads per block (=1024 threads in total which is the hardware limit)
-            const dim3 dimGrid  = dim3(std::ceil(this->host.m_nonMeshData.width  / static_cast<float>(dimBlock.x)),
-                                       std::ceil(this->host.m_nonMeshData.height / static_cast<float>(dimBlock.y)));
+            const dim3 dimGrid  = dim3(std::ceil(this->host.m_renderParams.width  / static_cast<float>(dimBlock.x)),
+                                       std::ceil(this->host.m_renderParams.height / static_cast<float>(dimBlock.y)));
     
             // trace rays through each pixel
-            ::PRTX::details::RayTracingDispatcher<<<dimGrid, dimBlock>>>(this->device.m_frameBuffer.GetData(), this->device.m_pNonMeshData);
+            ::PRTX::details::RayTracingDispatcher<<<dimGrid, dimBlock>>>(this->device.m_frameBuffer.GetData(), this->device.m_pRenderParams);
         
             // wait for the job to finish
             cudaDeviceSynchronize();
@@ -122,7 +89,7 @@ namespace PRTX {
         }
 
         inline ~PolarTracer() {
-            ::PRTX::Free(this->device.m_pNonMeshData);
+            ::PRTX::Free(this->device.m_pRenderParams);
         }
     }; // PolarTracer
 
