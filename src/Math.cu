7130358cc2d5cd65256bb34b44db3a71609c7f4a
Henry LE BERRE <24208000+henryleberre@users.noreply.github.com>
2020-10-21 22:14:25 +0200

    Added Vector::Length, Normalize & DotProduct

--- src/Math.cu
+++ src/Math.cu
@@ -6,6 +6,8 @@
 
 namespace PRTX {
 
+    const float EPSILON = 0.01f;
+
     template <typename _T>
     struct Vec4 {
         _T x, y, z, w;
@@ -14,6 +16,13 @@ namespace PRTX {
         __host__ __device__ inline Vec4(const _H& x = 0, const _V& y = 0, const _K& z = 0, const _Q& w = 0) noexcept {
             this->x = static_cast<_T>(x); this->y = static_cast<_T>(y); this->z = static_cast<_T>(z); this->w = static_cast<_T>(w);
         }
+
+
+        __host__ __device__ inline float GetLength3D() const noexcept { return sqrt(this->x*this->x+this->y*this->y+this->z*this->z); }
+        __host__ __device__ inline float GetLength4D() const noexcept { return sqrt(this->x*this->x+this->y*this->y+this->z*this->z+this->w*this->w); }
+
+        __host__ __device__ inline void Normalize3D() noexcept { this->operator/=(this->GetLength3D()); }
+        __host__ __device__ inline void Normalize4D() noexcept { this->operator/=(this->GetLength4D()); }
 
         template <typename _U>
         __host__ __device__ inline Vec4<_T>& operator+=(const Vec4<_U>& o) noexcept {
@@ -62,6 +71,24 @@ namespace PRTX {
         __host__ __device__ inline Vec4<_T>& operator/=(const _U& n) noexcept {
           this->x /= n; this->y /= n; this->z /= n; this->w /= n;
           return *this;
+        }
+
+        static __host__ __device__ inline float DotProduct3D(const Vec4<_T>& a, const Vec4<_T>& b) noexcept {
+          return a.x * b.x + a.y * b.y + a.z * b.z;
+        }
+
+        static __host__ __device__ inline float DotProduct4D(const Vec4<_T>& a, const Vec4<_T>& b) noexcept {
+          return a.x * b.x + a.y * b.y + a.z * b.z + a.w * b.w;
+        }
+
+        static __host__ __device__ inline Vec4<_T> Normalized3D(const Vec4<_T> v) noexcept {
+          v.Normalize3D();
+          return v;
+        }
+
+        static __host__ __device__ inline Vec4<_T> Normalized4D(const Vec4<_T> v) noexcept {
+          v.Normalize4D();
+          return v;
         }
     }; // Vec4<_T>
 
