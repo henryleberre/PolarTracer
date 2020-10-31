b5ab9a5167ce5584fe25a89a09a8e2b148542e4e
Henry LE BERRE <24208000+henryleberre@users.noreply.github.com>
2020-10-31 12:28:12 +0100

    Added Status Messages

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
+echo "Compiling..."
 nvcc -O3 -ccbin "/usr/bin/clang++" -std=c++17 src/PolarTracer.cu -o PolarTracer
+echo "Running..."
 ./PolarTracer
+echo "Converting & Deleting..."
 convert frame.pam frame.png
 rm frame.pam
 eog frame.png > /dev/null 2>&1
