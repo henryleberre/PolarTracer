861c4f97f4b02ae7e5645ceb1bcfe9ceab3e0161
PolarToCartesian <24208000+PolarToCartesian@users.noreply.github.com>
2020-11-01 16:12:52 +0100

    Working Triangle Rendering

--- src/PolarTracer.cu
+++ src/PolarTracer.cu
@@ -18,8 +18,8 @@
 // +--------------------+
 
 __constant__ const float  EPSILON = 0.0001f;
-__constant__ const size_t MAX_REC = 1u;
-__constant__ const size_t SPP     = 3u;
+__constant__ const size_t MAX_REC = 10u;
+__constant__ const size_t SPP     = 1000u;
 
 // The "Device" enum class represents the devices from which
 // memory can be accessed. This is necessary because the cpu can't
@@ -820,60 +820,44 @@ __device__ Intersection Ray::Intersects(const Plane& plane) const noexcept {
     return Intersection::MakeNullIntersection(*this);
 }
 
-// https://en.wikipedia.org/wiki/M%C3%B6ller%E2%80%93Trumbore_intersection_algorithm#:~:text=The%20M%C3%B6ller%E2%80%93Trumbore%20ray%2Dtriangle,the%20plane%20containing%20the%20triangle.
+//https://www.scratchapixel.com/lessons/3d-basic-rendering/ray-tracing-rendering-a-triangle
 template <>
 __device__ Intersection Ray::Intersects(const Triangle& triangle) const noexcept {
-    // compute plane's normal
-    Vec4f32 v0v1 = triangle.v1 - triangle.v0; 
-    Vec4f32 v0v2 = triangle.v2 - triangle.v0; 
-    // no need to normalize
-    Vec4f32 N = Vec4f32::CrossProduct3D(v0v1, v0v2); // N 
-    float area2 = N.GetLength3D(); 
+    float u = 0.f, v = 0.f;
+
+    const Vec4f32 v0v1 = triangle.v1 - triangle.v0; 
+    const Vec4f32 v0v2 = triangle.v2 - triangle.v0; 
+    const Vec4f32 a = this->direction;
+    const Vec4f32 b = v0v2;
+    const Vec4f32 pvec = {
+        a.y*b.z-a.z*b.y,
+        a.z*b.x-a.x*b.z,
+        a.x*b.y-a.y*b.x,
+        0
+    }; 
+    const float det = Vec4f32::DotProduct3D(v0v1, pvec); 
+
+    // ray and triangle are parallel if det is close to 0
+    if (fabs(det) < EPSILON) return Intersection::MakeNullIntersection(*this);
+
+    const float invDet = 1.f / det; 
  
-    // Step 1: finding P
+    const Vec4f32 tvec = this->origin - triangle.v0; 
+    u = Vec4f32::DotProduct3D(tvec, pvec) * invDet; 
+    if (u < 0 || u > 1) return Intersection::MakeNullIntersection(*this); 
  
-    // check if ray and plane are parallel ?
-    float NdotRayDirection = Vec4f32::DotProduct3D(N, this->direction); 
-    if (fabs(NdotRayDirection) < EPSILON) // almost 0 
-        return Intersection::MakeNullIntersection(*this); // they are parallel so they don't intersect ! 
- 
-    // compute d parameter using equation 2
-    float d = Vec4f32::DotProduct3D(N, triangle.v0); 
- 
-    // compute t (equation 3)
-    float t = (Vec4f32::DotProduct3D(N, this->origin) + d) / NdotRayDirection; 
-    // check if the triangle is in behind the ray
-    if (t < 0) return Intersection::MakeNullIntersection(*this); // the triangle is behind 
- 
-    // compute the intersection point using equation 1
-    Vec4f32 P = this->origin + t * this->direction; 
- 
-    // Step 2: inside-outside test
-    Vec4f32 C; // vector perpendicular to triangle's plane 
- 
-    // edge 0
-    Vec4f32 edge0 = triangle.v1 - triangle.v0; 
-    Vec4f32 vp0 = P - triangle.v0; 
-    C = Vec4f32::DotProduct3D(edge0, vp0);
-    if (Vec4f32::DotProduct3D(N, C) < 0) return Intersection::MakeNullIntersection(*this); // P is on the right side 
- 
-    // edge 1
-    Vec4f32 edge1 = triangle.v2 - triangle.v1; 
-    Vec4f32 vp1 = P - triangle.v1; 
-    C = Vec4f32::DotProduct3D(edge1, vp1); 
-    if (Vec4f32::DotProduct3D(N, C) < 0) return Intersection::MakeNullIntersection(*this);; // P is on the right side 
- 
-    // edge 2
-    Vec4f32 edge2 = triangle.v0 - triangle.v2; 
-    Vec4f32 vp2   = P - triangle.v2; 
-    C = Vec4f32::DotProduct3D(edge2, vp2); 
-    if (Vec4f32::DotProduct3D(N, C) < 0) return Intersection::MakeNullIntersection(*this);; // P is on the right side; 
+    const Vec4f32 qvec = Vec4f32::CrossProduct3D(tvec, v0v1); 
+    v = Vec4f32::DotProduct3D(this->direction, qvec) * invDet; 
+    if (v < 0 || u + v > 1) return Intersection::MakeNullIntersection(*this); 
  
     Intersection intersection;
+    intersection.t        = Vec4f32::DotProduct3D(v0v2, qvec) * invDet;
+
+    if (intersection.t <= 0) return Intersection::MakeNullIntersection(*this); 
+
     intersection.inRay    = *this;
-    intersection.t        = t;
-    intersection.location = P;
-    intersection.normal   = N;//TODO:
+    intersection.location = this->origin + intersection.t * this->direction;
+    intersection.normal   = Vec4f32(0.f, 0.f, 0.f, 0.f);
     intersection.material = triangle.material;
 
     return intersection;
@@ -1062,6 +1046,8 @@ public:
     }
 }; // PolarTracer
 
+#include <sstream>
+
 CPU_Array<Triangle> LoadObjectFile(const char* filename, const Material& material) {
     FILE* fp = fopen(filename, "r");
 
@@ -1076,22 +1062,24 @@ CPU_Array<Triangle> LoadObjectFile(const char* filename, const Material& materia
         switch (lineBuffer[0]) {
         case 'v':
         {
+            std::istringstream lineStream(lineBuffer + 1);
             Vec4f32 v;
-            std::fscanf(fp, "v %f %f %f", &v.x, &v.y, &v.z);
+            lineStream >> v.x >> v.y >> v.z;
             vertices.Append(v);
             break;
         }
         case 'f':
         {
-            unsigned int index0, index1, index2;
-            std::fscanf(fp, "f %u %u %u", &index0, &index1, &index2);
-            Triangle t;
-            t.v0 = vertices[index0 - 1];
-            t.v1 = vertices[index1 - 1];
-            t.v2 = vertices[index2 - 1];
-            t.material = material;
+            size_t vIndex0, vIndex1, vIndex2;
+            std::istringstream lineStream(lineBuffer + 1);
+            lineStream >> vIndex0 >> vIndex1 >> vIndex2;
+            Triangle tr;
+            tr.v0 = vertices[vIndex0 - 1];
+            tr.v1 = vertices[vIndex1 - 1];
+            tr.v2 = vertices[vIndex2 - 1];
+            tr.material = material;
 
-            triangles.Append(t);
+            triangles.Append(tr);
             break;
         }
         }
@@ -1112,12 +1100,13 @@ int main(int argc, char** argv) {
 
     renderParams.width  = WIDTH;
     renderParams.height = HEIGHT;
-    renderParams.camera.position = Vec4f32(0.f, .0f, -5.f, 0.f);
+    renderParams.camera.position = Vec4f32(0.f, .0f, -2.f, 0.f);
     renderParams.camera.fov      = 3.141592f / 4.f;
 
     Primitives<Array, Device::CPU> primitives;
-    primitives.spheres = CPU_Array<Sphere>(2);
+    primitives.spheres = CPU_Array<Sphere>(0);
     primitives.planes  = CPU_Array<Plane>(5);
+    primitives.triangles = CPU_Array<Triangle>(0);
 
     for (auto& o : primitives.spheres) {
         o.material.reflectance = 0.f;
@@ -1133,29 +1122,17 @@ int main(int argc, char** argv) {
         o.material.index_of_refraction = 1.0f;
     }
 
-    for (auto& o : primitives.triangles) {
-        o.material.reflectance = 0.f;
-        o.material.roughness = 1.0f;
-        o.material.transparency = 0.f;
-        o.material.index_of_refraction = 1.0f;
-    }
 
-    primitives.spheres[0].center = Vec4f32{ 0.0f, 1.5f, 0.5f, 0.f };
-    primitives.spheres[0].radius = 0.5f;
-    primitives.spheres[0].material.diffuse   = Colorf32{ 1.f, 1.f, 1.f, 1.f };
-    const float li = 5.f;
-    primitives.spheres[0].material.emittance = Colorf32{ li, li, li, 1.f };
-
-    primitives.spheres[1].center = Vec4f32{ 0.0f, 0.3f, 1.0f, 0.f };
-    primitives.spheres[1].radius = 0.5f;
-    primitives.spheres[1].material.diffuse   = Colorf32{ 1.f, 0.6f, 0.3f, 1.f };
-    primitives.spheres[1].material.emittance = Colorf32{ 0.f, 0.f, 0.f, 1.f };
-    primitives.spheres[1].material.reflectance = 0.1f;
+    //primitives.spheres[1].center = Vec4f32{ 0.0f, 0.3f, 1.0f, 0.f };
+    //primitives.spheres[1].radius = 0.5f;
+    //primitives.spheres[1].material.diffuse   = Colorf32{ 1.f, 0.6f, 0.3f, 1.f };
+    //primitives.spheres[1].material.emittance = Colorf32{ 0.f, 0.f, 0.f, 1.f };
+    //primitives.spheres[1].material.reflectance = 0.1f;
 
     primitives.planes[0].position = Vec4f32{ 0.f, -.25f, 0.f, 0.f};
     primitives.planes[0].normal   = Vec4f32{ 0.f, 1.f, 0.f, 0.f};
     primitives.planes[0].material.diffuse   = Colorf32{1.f, 1.f, 1.f, 1.f};
-    primitives.planes[0].material.emittance = Colorf32{0.f, 0.f, 0.f, 1.f};
+    primitives.planes[0].material.emittance = Colorf32{0.5f, 0.5f, 0.5f, 1.f};
 
     primitives.planes[1].position = Vec4f32{ 0.f, 0.f, 1.f, 0.f};
     primitives.planes[1].normal   = Vec4f32{ 0.f, 0.f, -1.f, 0.f};
@@ -1165,34 +1142,35 @@ int main(int argc, char** argv) {
     primitives.planes[1].material.roughness   = 0.f;
 
     primitives.planes[2] = primitives.planes[1];
-    primitives.planes[2].position = Vec4f32{1.f, 0.f, 0.f, 0.f};
+    primitives.planes[2].position = Vec4f32{1.2f, 0.f, 0.f, 0.f};
     primitives.planes[2].normal   = Vec4f32{-1.f, 0.f, 0.f, 0.f};
     primitives.planes[2].material.roughness   = 0.3f;
-    primitives.planes[2].material.reflectance = 0.8f;
+    primitives.planes[2].material.reflectance = 1.0f;
 
     primitives.planes[3] = primitives.planes[1];
-    primitives.planes[3].position = Vec4f32{-1.f, 0.f, 0.f, 0.f};
+    primitives.planes[3].position = Vec4f32{-1.2f, 0.f, 0.f, 0.f};
     primitives.planes[3].normal   = Vec4f32{1.f, 0.f, 0.f, 0.f};
-    primitives.planes[3].material.roughness = 0.1f;
+    primitives.planes[3].material.roughness   = 0.0f;
+    primitives.planes[3].material.reflectance = 1.0f;
 
     primitives.planes[4].position = Vec4f32{ 0.f, 0.f, renderParams.camera.position.z - 1.f, 0.f};
     primitives.planes[4].normal   = Vec4f32{ 0.f, 0.f, 1.f, 0.f};
     primitives.planes[4].material.diffuse   = Colorf32{.75f, .75f, .75f, 1.f};
-    primitives.planes[4].material.emittance = Colorf32{0.f, 0.f, 0.f, 1.f};
-
-    //float a = 0.25f;
-    //primitives.triangles[0].p0 = Vec4f32{ -a,  +a,  2.f, 0.f};
-    //primitives.triangles[0].p1 = Vec4f32{ +a,  +a,  2.f, 0.f};
-    //primitives.triangles[0].p2 = Vec4f32{ 0.f, 0.f, 2.f, 0.f};
-    //primitives.triangles[0].material.diffuse   = Colorf32{1.f, 1.f, 1.f, 1.f};
-    //primitives.triangles[0].material.emittance = Colorf32{1.f, 1.f, 1.f, 1.f};
+    primitives.planes[4].material.emittance = Colorf32{1.f, 1.f, 1.f, 1.f};
 
     Material bunnyMaterial;
     bunnyMaterial.diffuse   = Colorf32{.75f, .75f, .75f, 1.f};
-    bunnyMaterial.emittance = Colorf32{1.f, 1.f, 1.f, 1.f};
+    bunnyMaterial.emittance = Colorf32{0.f, 0.f, 0.f, 1.f};
+    bunnyMaterial.reflectance = 0.f;
+    bunnyMaterial.roughness = 1.0f;
+    bunnyMaterial.transparency = 0.f;
+    bunnyMaterial.index_of_refraction = 1.0f;
 
     auto bunnyTriangles = LoadObjectFile("res/bunny.obj", bunnyMaterial);
     for (auto tr : bunnyTriangles) {
+        tr.v0 *= 10; tr.v0.y -= 0.5f; tr.v0.z *= -1.f; tr.v0.x *= -1.f; tr.v0.x -= 0.1f;
+        tr.v1 *= 10; tr.v1.y -= 0.5f; tr.v1.z *= -1.f; tr.v1.x *= -1.f; tr.v0.x -= 0.1f;
+        tr.v2 *= 10; tr.v2.y -= 0.5f; tr.v2.z *= -1.f; tr.v2.x *= -1.f; tr.v0.x -= 0.1f;
         primitives.triangles.Append(tr);
     }
 
